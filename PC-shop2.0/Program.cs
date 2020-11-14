a0c57422fd455ea42decd7c7e972d16bfc0fe687
Yoran De Smet <yoran.desmet@student.romerocollege.be>
2020-11-14 13:37:16 +0100

    Add project files.

---
+++ PC-shop2.0/Program.cs
@@ -0,0 +1,40 @@
+﻿/* 5AIT3 - Yoran - 2020-11-14
+ * 
+ * Vraag volgende zaken aan gebruiker in context van PC-shop:
+ * Artikel ? Monitor
+ * Eenheidsprijs ? 169,5
+ * Aantal stuks ? 2
+ * Onderneming [j/n]? ...
+ * 
+ * Toon volgende output indien onderneming=n:
+ * 2 stuks MONITOR
+ * Kostprijs: 339,00 €
+ * 
+ * Toon volgende output indien onderneming=j:
+ * 2 stuks MONITOR
+ * Kostprijs: 280,02 € (BTW: 58,98 €)
+ * 
+ * ANALYSE:
+ * --------
+ * VRAAG    Monitor
+ * VRAAG    Eenheidsprijs
+ * VRAAG 
+ * VRAAG 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_shop2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
